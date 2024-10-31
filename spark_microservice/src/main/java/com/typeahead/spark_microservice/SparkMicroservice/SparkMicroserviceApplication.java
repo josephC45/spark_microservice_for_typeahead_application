7b697fc3c62e9e6cf64805df781b9f8b89f3fd41
josephC45 <joey.curtog@gmail.com>
2024-10-30 21:52:57 -0400

    add project files

---
+++ spark_microservice/src/main/java/com/typeahead/spark_microservice/SparkMicroservice/SparkMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.typeahead.spark_microservice.SparkMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkMicroserviceApplication.class, args);
+	}
+
+}
