21cac0c5b922552e1facb30b747c518483cfd0e2
josephC45 <joey.curtog@gmail.com>
2024-11-11 18:02:03 -0500

    delete unnecessary KafkaConsumerService interface and KafkaConsumerServiceImpl class

--- spark_microservice/src/main/java/com/typeahead/spark_microservice/SparkMicroservice/service/KafkaConsumerService.java
+++
@@ -1,7 +0,0 @@
-package com.typeahead.spark_microservice.SparkMicroservice.service;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-public interface KafkaConsumerService {
-    public void listenToKafkaTopic(ConsumerRecord<String, String> record);
-}
