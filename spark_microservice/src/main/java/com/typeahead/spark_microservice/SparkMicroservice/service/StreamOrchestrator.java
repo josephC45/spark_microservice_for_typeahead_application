6107e41c1da7eaca3595e0775d136e6420dd1259
josephC45 <joey.curtog@gmail.com>
2025-02-20 20:11:42 -0500

    refactor SparkStreamOrchestrator into StreamOrchestrator for managing data flow between consumer and storage

---
+++ spark_microservice/src/main/java/com/typeahead/spark_microservice/SparkMicroservice/service/StreamOrchestrator.java
@@ -0,0 +1,16 @@
+package com.typeahead.spark_microservice.SparkMicroservice.service;
+//TODO Maybe consider mediator pattern?
+public abstract class StreamOrchestrator<T, P> {
+
+    private final ConsumerService<T, P> consumerService;
+
+    StreamOrchestrator(ConsumerService<T, P> consumerService) {
+        this.consumerService = consumerService;
+    }
+
+    abstract void sendToStorage(T wordAndWordCountStream);
+
+    public void start() {
+        sendToStorage(consumerService.currentWordStream());
+    }
+}
