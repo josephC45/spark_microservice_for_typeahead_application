44843f04b9cfcc7949f81d093fabb87271b50074
josephC45 <joey.curtog@gmail.com>
2025-02-20 20:17:25 -0500

    rename/delete files

--- spark_microservice/src/main/java/com/typeahead/spark_microservice/SparkMicroservice/service/SparkKafkaStreamService.java
+++
@@ -1,8 +0,0 @@
-package com.typeahead.spark_microservice.SparkMicroservice.service;
-
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-
-public interface SparkKafkaStreamService {
-    public Dataset<Row> createKafkaConsumerStream();
-}
