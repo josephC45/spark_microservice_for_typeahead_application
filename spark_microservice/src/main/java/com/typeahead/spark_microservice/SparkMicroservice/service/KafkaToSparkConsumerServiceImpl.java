7074a312fcef76e756cef9a6619a019a91c8dce9
josephC45 <joey.curtog@gmail.com>
2025-02-20 20:09:22 -0500

    refactor SparkKafkaStreamServiceImpl into KafkaToSparkConsumerServiceImpl for stream ingestion

---
+++ spark_microservice/src/main/java/com/typeahead/spark_microservice/SparkMicroservice/service/KafkaToSparkConsumerServiceImpl.java
@@ -0,0 +1,41 @@
+package com.typeahead.spark_microservice.SparkMicroservice.service;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.typeahead.spark_microservice.SparkMicroservice.configuration.ApplicationConfig;
+
+@Service
+class KafkaToSparkConsumerServiceImpl extends ConsumerService<Dataset<Row>, ApplicationConfig> {
+    final SparkSession sparkSession;
+
+    KafkaToSparkConsumerServiceImpl(@Qualifier("KafkaConfig") ApplicationConfig consumerConfig) {
+        super(consumerConfig);
+        this.sparkSession = SparkSession.builder()
+                .appName("StructuredWordAggregator")
+                .master("local[1]")
+                .getOrCreate();
+    }
+
+    @Override
+    Dataset<Row> ingestWordStream() {
+        return sparkSession
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", properties.getBootstrapServers())
+                .option("subscribe", properties.getTopic())
+                .option("startingOffsets", properties.getOffset())
+                .load();
+    }
+
+    @Override
+    Dataset<Row> aggregateByWord(Dataset<Row> ingestedStream) {
+        return ingestedStream
+                .selectExpr("CAST(value AS STRING) AS word")
+                .groupBy("word")
+                .count();
+    }
+}
