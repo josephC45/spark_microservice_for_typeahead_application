6d41877437a71d79ee3b6757236a5392cfce3a38
josephC45 <joey.curtog@gmail.com>
2025-02-20 20:24:16 -0500

    add SparkStreamOrchestractor to manage data flow between consumer (kafka) and storage (hbase)

--- spark_microservice/src/main/java/com/typeahead/spark_microservice/SparkMicroservice/service/SparkStreamOrchestrator.java
+++ spark_microservice/src/main/java/com/typeahead/spark_microservice/SparkMicroservice/service/SparkStreamOrchestrator.java
@@ -1,5 +1,24 @@
 package com.typeahead.spark_microservice.SparkMicroservice.service;
 
-public interface SparkStreamOrchestrator {
-    public void start();
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.springframework.stereotype.Service;
+
+import com.typeahead.spark_microservice.SparkMicroservice.configuration.ApplicationConfig;
+
+@Service
+class SparkStreamOrchestrator extends StreamOrchestrator<Dataset<Row>, ApplicationConfig> {
+
+    private final UploadToStorageService<Dataset<Row>, ApplicationConfig> uploadToStorageService;
+
+    SparkStreamOrchestrator(ConsumerService<Dataset<Row>, ApplicationConfig> kafkaToSparkConsumerService,
+            UploadToStorageService<Dataset<Row>, ApplicationConfig> uploadToStorageService) {
+        super(kafkaToSparkConsumerService);
+        this.uploadToStorageService = uploadToStorageService;
+    }
+
+    @Override
+    void sendToStorage(Dataset<Row> wordAndWordCountStream) {
+        uploadToStorageService.upload(wordAndWordCountStream); //TODO Create concrete impl
+    }
 }
